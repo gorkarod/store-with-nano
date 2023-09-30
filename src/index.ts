a11c8368d95451e2a0b87d4fa13b4965983291f7
Hartmut Junker <mail@hartmutjunker.de>
2023-09-30 11:24:04 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import './style.css'
+
+import { apiuri } from './stores.ts'
+
+console.log(apiuri.get())
---
+++ src/stores.ts
@@ -0,0 +1,3 @@
+import { atom } from 'nanostores'
+
+export const apiuri = atom<string>('http:77api.com')
